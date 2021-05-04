a05a2b4a057e4aecd0e0962700ead181c7e3efd8
Casey Wood <caseywood252@gmail.com>
2021-05-04 19:19:23 +0000

    first porject ( objects )

---
+++ objects.js
@@ -0,0 +1,48 @@
+/*
+var myObj = { 
+    color: "orange",
+    shape: "sphere",
+    type: ["food", 'apple'], 
+    eat: function() { return "yummy" }
+  };
+  
+  console.log(myObj.type)
+
+
+console.log('this is \n a test')
+*/
+
+/*
+
+// example of a more complex object 
+
+var facebookProfile = {
+    name: 'Casey Wood',
+    friends: 650,
+    messages:['start', 'mid', 'lunch', 'supper'],
+    addFriend: function(){
+        facebookProfile.friends += 1
+    },
+    removeFriend: function(){
+        return facebookProfile.friends -= 1
+    },
+    postMessage: function(tweet){
+        return facebookProfile.messages.push(tweet) 
+    },
+    deleteMessage: function(loc){
+        return facebookProfile.messages.splice(loc, 1) 
+        
+    },
+}
+// console.log(facebookProfile.messages['mid'])
+console.log(facebookProfile.deleteMessage())
+console.log(facebookProfile.messages)
+*/
+
+const pizza = {  
+    toppings: ['cheese', 'sauce', 'pepperoni'],  
+    crust: 'deep dish',  
+    serves: 2  
+  } 
+
+console.log(pizza)
